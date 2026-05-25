74692548589e486e0344c44e495e80e834c40cc1
landowark <lando.wark@gmail.com>
2026-05-25 08:44:54 -0500

    Writing new tests.

--- tests/backend/managers/test_manager_basic.py
+++
@@ -1,8 +0,0 @@
-import pytest, sys
-if "C:\\Users\\lwark\\Documents\\python\\submissions\\src\\submissions" not in sys.path:
-    sys.path.append("C:\\Users\\lwark\\Documents\\python\\submissions\\src\\submissions")
-from backend import managers
-
-def test_create_default_manager():
-    with pytest.raises(NotImplementedError, match="Parse only implemented in subclasses."):
-        managers.DefaultManager(None, r"tests\resources\226C4100.xlsx") 
